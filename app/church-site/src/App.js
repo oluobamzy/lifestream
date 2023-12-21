c4f81ac8c55c39c748a5ae2680120dea73798165
oluobamzy <oluobamzy@gmail.com>
2023-12-21 00:16:35 -0500

    setting up React

---
+++ app/church-site/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import SplashScreen from './Home/SplashScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SplashScreen />
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ app/church-site/src/Home/SplashScreen.js
@@ -0,0 +1,9 @@
+const SplashScreen = () => {
+  return (
+    <div className="splash-screen">
+      <h1>Church Name</h1>
+      <p>Tagline</p>
+      </div>
+  )
+};
+export default SplashScreen;
