82612c4ea3a377dc377dfc4ceff70b4af01183a1
oluobamzy <oluobamzy@gmail.com>
2023-12-21 23:47:09 -0500

    adding Mission statement

---
+++ app/church-site/src/Home/HomeButton.js
@@ -0,0 +1,9 @@
+import "./Home.css";
+const HomeButton = () => {
+    return (
+        <div>
+            <button>Join Us</button>
+        </div>
+    );
+};
+export default HomeButton;
---
+++ app/church-site/src/Mission/MissionImg.js
@@ -0,0 +1,24 @@
+const MissionImg = () => {
+  return (
+    <div className="mission-img">
+      <div className="mission-img-card">
+        <img src="https://www.brooksidepc.org/wp-content/uploads/2020/09/congregational_singing_church.jpeg" alt="mission-img" />
+      </div>
+      <div className="mission-img-text">
+        <h1>Our Mission</h1>
+        <p>
+          is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer
+          took a galley of type and scrambled it to make a type
+          specimen book. It has survived not only five centuries, but
+          also the leap into electronic typesetting, remaining
+          essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum
+          passages
+        </p>
+      </div>
+    </div>
+  );
+};
+export default MissionImg;
